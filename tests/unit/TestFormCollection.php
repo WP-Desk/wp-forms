6f943d51bec1752e85934c471ab5777c841eea7e
Marcin Kolanko <marcin@inspirelabs.pl>
2019-12-17 13:19:24 +0000

    MR changes

--- tests/unit/TestFormCollection.php
+++ tests/unit/TestFormCollection.php
@@ -5,26 +5,29 @@ namespace Tests;
 use WPDesk\Forms\AbstractForm;
 use WPDesk\Forms\FormsCollection;
 
-class TestFormCollection extends \PHPUnit\Framework\TestCase
-{
+class TestFormCollection extends \PHPUnit\Framework\TestCase {
 
 	const FORM1_ID = 'test_form';
 	const FORM2_ID = 'test_form2';
 
-	const TEST_ARRAY1 = [ 'test' => true ];
-	const TEST_ARRAY2 = [ 'test2' => 'potato' ];
+	const FORM1_FORM_DATA = [ 'test' => true ];
+	const FORM2_FORM_DATA = [ 'test2' => 'potato' ];
+
+	const FORM1_PREFIXED_FORM_DATA = [ 'test_form_test' => true ];
+	const FORM1_UPDATED_FORM_DATA  = [ 'test666' => true ];
+
 
 	private $formConditionalTrue;
 	private $formConditionalFalse;
 
-	protected function setUp(){
+	protected function setUp() {
 		$this->formConditionalTrue = $this->getMockBuilder( AbstractForm::class )
 		                                  ->enableOriginalConstructor()
 		                                  ->setMethods( [ 'get_form_id', 'is_active' ] )
 		                                  ->getMockForAbstractClass();
 		$this->formConditionalTrue->method( 'get_form_id' )->willReturn( self::FORM1_ID );
 		$this->formConditionalTrue->method( 'is_active' )->willReturn( true );
-		$this->formConditionalTrue->method( 'create_form_data' )->willReturn( self::TEST_ARRAY1 );
+		$this->formConditionalTrue->method( 'create_form_data' )->willReturn( self::FORM1_FORM_DATA );
 
 		$this->formConditionalFalse = $this->getMockBuilder( AbstractForm::class )
 		                                   ->enableOriginalConstructor()
@@ -32,48 +35,45 @@ class TestFormCollection extends \PHPUnit\Framework\TestCase
 		                                   ->getMockForAbstractClass();
 		$this->formConditionalFalse->method( 'get_form_id' )->willReturn( self::FORM2_ID );
 		$this->formConditionalFalse->method( 'is_active' )->willReturn( false );
-		$this->formConditionalFalse->method( 'create_form_data' )->willReturn( self::TEST_ARRAY2 );
+		$this->formConditionalFalse->method( 'create_form_data' )->willReturn( self::FORM2_FORM_DATA );
 	}
 
-	protected function getFormConditionalTrue(){
+	protected function getFormConditionalTrue() {
 		return clone $this->formConditionalTrue;
 	}
 
-	protected function getFormConditionalFalse(){
+	protected function getFormConditionalFalse() {
 		return clone $this->formConditionalFalse;
 	}
 
 	/**
 	 * Test adding and checking single form.
 	 */
-	public function testIfFormExists()
-	{
+	public function testIfFormExists() {
 		$collection = new FormsCollection();
 		$collection->add_form( $this->getFormConditionalTrue() );
 
 		$this->assertTrue( $collection->is_form_exists( self::FORM1_ID ) );
 	}
 
-    /**
-     * Test adding and checking multiple forms.
-     */
-    public function testIfFormsExists()
-    {
-    	$collection = new FormsCollection();
-    	$collection->add_forms([
-    		$this->getFormConditionalTrue(),
-		    $this->getFormConditionalFalse(),
-	    ]);
+	/**
+	 * Test adding and checking multiple forms.
+	 */
+	public function testIfFormsExists() {
+		$collection = new FormsCollection();
+		$collection->add_forms( [
+			$this->getFormConditionalTrue(),
+			$this->getFormConditionalFalse(),
+		] );
 
-	    $this->assertTrue( $collection->is_form_exists( self::FORM1_ID ) );
-	    $this->assertTrue( $collection->is_form_exists( self::FORM2_ID ) );
-    }
+		$this->assertTrue( $collection->is_form_exists( self::FORM1_ID ) );
+		$this->assertTrue( $collection->is_form_exists( self::FORM2_ID ) );
+	}
 
 	/**
 	 * Test getting single form. AbstractForm object is expected
 	 */
-	public function testGettingExistingForm()
-	{
+	public function testGettingExistingForm() {
 		$collection = new FormsCollection();
 		$collection->add_form( $this->getFormConditionalTrue() );
 
@@ -86,12 +86,11 @@ class TestFormCollection extends \PHPUnit\Framework\TestCase
 	/**
 	 * Test getting not existing single form.
 	 */
-	public function testGettingNotExistingForm()
-	{
+	public function testGettingNotExistingForm() {
 		$collection = new FormsCollection();
 		$collection->add_form( $this->getFormConditionalTrue() );
 
-		$this->expectException(\OutOfRangeException::class);
+		$this->expectException( \OutOfRangeException::class );
 		$collection->get_form( '123456' );
 	}
 
@@ -105,7 +104,7 @@ class TestFormCollection extends \PHPUnit\Framework\TestCase
 			$this->getFormConditionalFalse(),
 		] );
 
-		$this->assertSame( self::TEST_ARRAY1, $collection->get_forms_data() );
+		$this->assertSame( self::FORM1_FORM_DATA, $collection->get_forms_data() );
 	}
 
 	/**
@@ -118,7 +117,7 @@ class TestFormCollection extends \PHPUnit\Framework\TestCase
 			$this->getFormConditionalFalse(),
 		] );
 
-		$this->assertSame( [ 'test_form_test' => true ], $collection->get_forms_data( true ) );
+		$this->assertSame( self::FORM1_PREFIXED_FORM_DATA, $collection->get_forms_data( true ) );
 
 
 	}
@@ -133,10 +132,9 @@ class TestFormCollection extends \PHPUnit\Framework\TestCase
 			$this->getFormConditionalFalse(),
 		] );
 
-		$test_array = [ 'test666' => true ];
-		$form       = $collection->get_form( self::FORM1_ID );
-		$form->update_form_data( $test_array );
-		$this->assertSame( array_merge( self::TEST_ARRAY1, $test_array ), $collection->get_forms_data() );
+		$form = $collection->get_form( self::FORM1_ID );
+		$form->update_form_data( self::FORM1_UPDATED_FORM_DATA );
+		$this->assertSame( array_merge( self::FORM1_FORM_DATA, self::FORM1_UPDATED_FORM_DATA ), $collection->get_forms_data() );
 
 	}
 
