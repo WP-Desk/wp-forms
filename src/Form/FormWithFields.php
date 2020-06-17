a3d016b393493510b029fc751a62dc3e47471019
dyszczo <krzysztof.dyszczyk@gmail.com>
2020-06-17 17:12:40 +0100

    fixed formatting

--- src/Form/FormWithFields.php
+++ src/Form/FormWithFields.php
@@ -172,7 +172,9 @@ class FormWithFields implements Form, ContainerForm, FieldProvider {
 		foreach ( $this->get_fields() as $field ) {
 			$data_key = $field->get_name();
 
-			if( empty($data_key) ) continue;
+			if ( empty( $data_key ) ) {
+				continue;
+			}
 
 			if ( ! isset( $this->updated_data[ $data_key ] ) ) {
 				$container->set( $data_key, $field->get_default_value() );
