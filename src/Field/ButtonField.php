df9acbc3e12018155e7ba647dbb1a3153c807ea1
Marcin Kolanko <marcin@inspirelabs.pl>
2020-06-24 12:42:39 +0000

    added button form field

---
+++ src/Field/ButtonField.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DropshippingXmlVendor\WPDesk\Forms\Field;
+
+class ButtonField extends \DropshippingXmlVendor\WPDesk\Forms\Field\NoValueField
+{
+    public function get_template_name()
+    {
+        return 'button';
+    }
+    public function get_type()
+    {
+        return 'button';
+    }
+}
