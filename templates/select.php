e0719cc658d9a633c2c999c183ca9ae90f5b5e56
Marcin Kolanko <marcin@inspirelabs.pl>
2020-08-18 10:10:54 +0000

    fix(Template): add square brackets for select multiple

--- templates/select.php
+++ templates/select.php
@@ -8,7 +8,7 @@
 <select
 	id="<?php echo esc_attr( $field->get_id() ); ?>"
 	<?php if ($field->has_classes()): ?>class="<?php echo esc_attr( $field->get_classes() ); ?>"<?php endif; ?>
-	name="<?php echo esc_attr( $name_prefix ); ?>[<?php echo esc_attr( $field->get_name() ); ?>]"
+	name="<?php echo esc_attr( $name_prefix ); ?>[<?php echo esc_attr( $field->get_name() ); ?>]<?php echo $field->is_multiple()? '[]' : ''; ?>"
 	<?php foreach ($field->get_attributes() as $key => $attr_val): ?>
 		<?php echo esc_attr($key); ?>="<?php echo esc_attr($attr_val); ?>"
 	<?php endforeach; ?>
