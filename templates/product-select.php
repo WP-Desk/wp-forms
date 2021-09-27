3b88833605add9208cd28e8fb9125eb41a68c542
Marcin Kolanko <marcin@inspirelabs.pl>
2021-09-27 13:14:32 +0200

    fix(templates): add escaping templates from wp-forms 3.0

--- templates/product-select.php
+++ templates/product-select.php
@@ -1,23 +1,26 @@
 <?php
-
 /**
  * @var \WPDesk\Forms\Field $field
  * @var string $name_prefix
  * @var string[] $value
  */
+
 ?>
 
 <select class="wc-product-search" multiple="multiple" style="width: 50%;"
-        id="<?php echo esc_attr( $field->get_id() ); ?>"
-        name="<?php echo esc_attr( $name_prefix ); ?>[<?php echo esc_attr( $field->get_name() ); ?>][]"
-        data-placeholder="<?php esc_attr_e( 'Search for a product&hellip;', 'woocommerce' ); ?>"
-        data-action="woocommerce_json_search_products_and_variations">
+		id="<?php echo \esc_attr( $field->get_id() ); ?>"
+		name="<?php echo \esc_attr( $name_prefix ); ?>[<?php echo \esc_attr( $field->get_name() ); ?>][]"
+		data-placeholder="<?php \esc_attr_e( 'Search for a product&hellip;', 'wp-forms' ); ?>"
+		data-action="woocommerce_json_search_products_and_variations">
 	<?php
 	foreach ( (array) $value as $product_id ) {
 		$product = wc_get_product( $product_id );
 		if ( is_object( $product ) ) {
-			echo '<option value="' . esc_attr( $product_id ) . '"' . selected( true, true,
-					false ) . '>' . wp_kses_post( $product->get_formatted_name() ) . '</option>';
+			echo '<option value="' . \esc_attr( $product_id ) . '"' . selected(
+				true,
+				true,
+				false
+			) . '>' . wp_kses_post( $product->get_formatted_name() ) . '</option>';
 		}
 	}
 	?>
