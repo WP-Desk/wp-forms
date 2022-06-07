5c6ca1f87efa092fc3fb919916d1cc58c825d881
Piotr Po <potreb@gmail.com>
2022-06-07 20:40:52 +0200

    fix: radio field

--- src/Field/RadioField.php
+++ src/Field/RadioField.php
@@ -4,6 +4,10 @@ namespace WPDesk\Forms\Field;
 
 class RadioField extends BasicField {
 
+	public function get_type(): string {
+		return 'radio';
+	}
+
 	public function get_template_name(): string {
 		return 'input-radio';
 	}
