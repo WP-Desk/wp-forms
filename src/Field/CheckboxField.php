aa248eb105b5a43a0b1f157942b0637dd68acf61
Piotr Potrebka <piotr.potrebka@wpdesk.net>
2023-02-02 08:04:04 +0100

    fix: unused const, type cast

--- src/Field/CheckboxField.php
+++ src/Field/CheckboxField.php
@@ -6,9 +6,6 @@ use WPDesk\Forms\Field;
 
 class CheckboxField extends BasicField {
 
-	const VALUE_TRUE  = 'yes';
-	const VALUE_FALSE = 'no';
-
 	public function get_type(): string {
 		return 'checkbox';
 	}
